86b6b97f30b0f88763694296ddbe8c4bb3042e05
anytizer <anytizer@bimal.org.np>
2019-08-28 12:34:52 -0600

    api

---
+++ public_html/api/contact.php
@@ -0,0 +1,14 @@
+<?php
+require_once "inc.config.php";
+
+file_put_contents("contacts.log", print_r($_POST, true));
+
+/**
+Array
+(
+    [fullname] => xa
+    [method] => adf
+    [message] => asd
+)
+
+*/
