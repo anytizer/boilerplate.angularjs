2711e7a8e2c5ef1eeab7a2d980fdec6e8adb9bfc
anytizer <anytizer@bimal.org.np>
2019-08-30 16:05:03 -0600

    formatting

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,217 +1,226 @@
 var myApp = angular.module("myApp", ["ui.router"]);
 
 myApp.factory("httpRequestInterceptor", function () {
-	return {
-		request: function (config) {
+    return {
+        request: function (config) {
 
-			config.headers["X-Protection-Token"] = "49E12FA8-C92D-11E9-B654-00360FAA29CD";
-			config.headers["Accept"] = "application/json;odata=verbose";
+            config.headers["X-Protection-Token"] = "49E12FA8-C92D-11E9-B654-00360FAA29CD";
+            config.headers["Accept"] = "application/json;odata=verbose";
 
-			return config;
-		}
-	};
+            return config;
+        }
+    };
 });
 
 
 myApp.config(function ($httpProvider) {
-	$httpProvider.interceptors.push("httpRequestInterceptor");
+    $httpProvider.interceptors.push("httpRequestInterceptor");
 });
 
 
-myApp.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise("/home");
-  
-  $stateProvider.state({
-    name: "home",
-    url: "/home",
-    templateUrl: "templates/home.html",
-	controller: "HomeController",
-  });
-  
-  $stateProvider.state({
-    name: "about",
-    url: "/about",
-    templateUrl: "templates/about.html",
-	controller: "AboutController",
-  });
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
 
-  $stateProvider.state({
-    name: "products",
-    url: "/products",
-    templateUrl: "templates/products.html",
-	controller: function($state){$state.go(".list");},
-  });
-  
-  $stateProvider.state({
-    name: "products.list",
-    url: "/list",
-    templateUrl: "templates/products-list.html",
-	controller: "ProductsListController",
-  });
-  
-  $stateProvider.state({
-    name: "products.details",
-    url: "/details/:id",
-    templateUrl: "templates/products-details.html",
-	controller: "ProductsDetailsController",
-  });
-  
-  $stateProvider.state({
-    name: "products.add",
-    url: "/add",
-    templateUrl: "templates/products-add.html",
-	controller: "ProductsAddController",
-  });
-  
-  $stateProvider.state({
-    name: "login",
-    url: "/login",
-    templateUrl: "templates/login.html",
-	controller: "LoginController",
-  });
-  
-  $stateProvider.state({
-    name: "register",
-    url: "/register",
-    templateUrl: "templates/register.html",
-	controller: "RegistrationController",
-  });
-  
-  $stateProvider.state({
-    name: "contact",
-    url: "/contact",
-    templateUrl: "templates/contact.html",
-	controller: "ContactController",
-  });
-  
+    $stateProvider.state({
+        name: "home",
+        url: "/home",
+        templateUrl: "templates/home.html",
+        controller: "HomeController",
+    });
+
+    $stateProvider.state({
+        name: "about",
+        url: "/about",
+        templateUrl: "templates/about.html",
+        controller: "AboutController",
+    });
+
+    $stateProvider.state({
+        name: "products",
+        url: "/products",
+        templateUrl: "templates/products.html",
+        controller: function ($state) {
+            $state.go(".list");
+        },
+    });
+
+    $stateProvider.state({
+        name: "products.list",
+        url: "/list",
+        templateUrl: "templates/products-list.html",
+        controller: "ProductsListController",
+    });
+
+    $stateProvider.state({
+        name: "products.details",
+        url: "/details/:id",
+        templateUrl: "templates/products-details.html",
+        controller: "ProductsDetailsController",
+    });
+
+    $stateProvider.state({
+        name: "products.add",
+        url: "/add",
+        templateUrl: "templates/products-add.html",
+        controller: "ProductsAddController",
+    });
+
+    $stateProvider.state({
+        name: "login",
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: "LoginController",
+    });
+
+    $stateProvider.state({
+        name: "register",
+        url: "/register",
+        templateUrl: "templates/register.html",
+        controller: "RegistrationController",
+    });
+
+    $stateProvider.state({
+        name: "contact",
+        url: "/contact",
+        templateUrl: "templates/contact.html",
+        controller: "ContactController",
+    });
+
 });
 
 
-myApp.service("APIService", ["$http", function($http) {
-	var api = "./api";
-	return {
-		"products": function(data){
-			return $http({
-				method: "POST",
-				url: api+"/products.php",
-				data: data
-			});
-		},
-		"products_add": function(data){
-			return $http({
-				method: "POST",
-				url: api+"/products-add.php",
-				data: data
-			});
-		},
-		"contact": function(data){
-			return $http({
-				method: "POST",
-				url: api+"/contact.php",
-				data: data
-			});
-		},
-	};
-}]);
+myApp.service("APIService", ["$http", function ($http) {
+        var api = "./api";
+        return {
+            "products": function (data) {
+                return $http({
+                    method: "POST",
+                    url: api + "/products.php",
+                    data: data
+                });
+            },
+            "products_add": function (data) {
+                return $http({
+                    method: "POST",
+                    url: api + "/products-add.php",
+                    data: data
+                });
+            },
+            "contact": function (data) {
+                return $http({
+                    method: "POST",
+                    url: api + "/contact.php",
+                    data: data
+                });
+            },
+        };
+    }]);
 
 
-myApp.controller("HomeController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	$scope.today = new Date();
-}]);
+myApp.controller("HomeController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        $scope.today = new Date();
+        $scope.galleries = [
+            {"src":"images/blank.png", "alt":""},
+            {"src":"images/blank.png", "alt":""},
+            {"src":"images/blank.png", "alt":""},
+            {"src":"images/blank.png", "alt":""},
+            {"src":"images/blank.png", "alt":""},
+        ];
+    }]);
 
 
-myApp.controller("AboutController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-}]);
+myApp.controller("AboutController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+    }]);
 
 
-myApp.controller("ProductsListController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	// @todo Read from API
-	$scope.products = [];
-	$scope.products_list = function(){
-		APIService.products()
-		.then(function(response){
-			$scope.products = response.data;
-		}, function(error){
-			alert("Error: "+error);
-		});
-	};
-	
-	$scope.products_list();
-}]);
+myApp.controller("ProductsListController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        // @todo Read from API
+        $scope.products = [];
+        $scope.products_list = function () {
+            APIService.products()
+                    .then(function (response) {
+                        $scope.products = response.data;
+                    }, function (error) {
+                        alert("Error: " + error);
+                    });
+        };
+
+        $scope.products_list();
+    }]);
 
 
-myApp.controller("ProductsDetailsController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	//alert("Getting details of: "+$stateParams.id);
-	$scope.id = $stateParams.id;
-}]);
+myApp.controller("ProductsDetailsController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        //alert("Getting details of: "+$stateParams.id);
+        $scope.id = $stateParams.id;
+    }]);
 
 
-myApp.controller("ProductsAddController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	$scope.product = {};
-	$scope.add = function(){
-		APIService.products_add($scope.product)
-		.then(function(response){
-			alert("Added successfully");
-			$scope.product = {};
-		}, function(error){
-			alert("Error saving product");
-			console.log(error);
-		});
-		
-		return false;
-	};
-}]);
+myApp.controller("ProductsAddController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        $scope.product = {};
+        $scope.add = function () {
+            APIService.products_add($scope.product)
+                    .then(function (response) {
+                        alert("Added successfully");
+                        $scope.product = {};
+                    }, function (error) {
+                        alert("Error saving product");
+                        console.log(error);
+                    });
+
+            return false;
+        };
+    }]);
 
 
-myApp.controller("RegistrationController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	$scope.registration = {"email": "",};
-	$scope.valid = function(){
-		return false;
-	};
-	$scope.register = function(registration){
-		alert("Registering..."+registration);
-	};
-}]);
+myApp.controller("RegistrationController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        $scope.registration = {"email": "", };
+        $scope.valid = function () {
+            return false;
+        };
+        $scope.register = function (registration) {
+            alert("Registering..." + registration);
+        };
+    }]);
 
 
-myApp.controller("LoginController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	$scope.credentials = {"email": "", "password": "",};
-	$scope.valid = function(){
-		return false;
-	};
-	$scope.login = function(){
-		alert("Logging in...");
-	};
-}]);
+myApp.controller("LoginController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        $scope.credentials = {"email": "", "password": "", };
+        $scope.valid = function () {
+            return false;
+        };
+        $scope.login = function () {
+            alert("Logging in...");
+        };
+    }]);
 
 
-myApp.controller("ContactController", ["$scope", "$state", "$stateParams", "APIService", function($scope, $state, $stateParams, APIService) {
-	// @todo Read from API
-	$scope.info = {
-		"company": "XYZ LLC.",
-		"address": "Address",
-		"phone": "000-000-0000",
-		"fax": "000-000-0000",
-	};
-	
-	$scope.form = {
-		"fullname": "",
-		"method": "",
-		"message": "",
-	};
-	
-	$scope.contact = function(){
-		APIService.contact($scope.form).then(function(response){
-			alert("Contacted successfully");
-			$scope.form = {
-				"fullname": "",
-				"method": "",
-				"message": "",
-			};
-		}, function(error){
-			alert("Error: "+error);
-		});
-		
-		return false;
-	};
-}]);
+myApp.controller("ContactController", ["$scope", "$state", "$stateParams", "APIService", function ($scope, $state, $stateParams, APIService) {
+        // @todo Read from API
+        $scope.info = {
+            "company": "XYZ LLC.",
+            "address": "Address",
+            "phone": "000-000-0000",
+            "fax": "000-000-0000",
+        };
+
+        $scope.form = {
+            "fullname": "",
+            "method": "",
+            "message": "",
+        };
+
+        $scope.contact = function () {
+            APIService.contact($scope.form).then(function (response) {
+                alert("Contacted successfully");
+                $scope.form = {
+                    "fullname": "",
+                    "method": "",
+                    "message": "",
+                };
+            }, function (error) {
+                alert("Error: " + error);
+            });
+
+            return false;
+        };
+    }]);
